aa5164c4cbd5ad375433c4da2e3d53ff49adeb1e
L1mpan <70134910+L1mpan@users.noreply.github.com>
2020-09-08 11:48:03 +0200

    Add files via upload

---
+++ inlamning1.py
@@ -0,0 +1,19 @@
+# Hej, jag kom inte på något sätt att köra bara en del av koden så jag använde mig av "#" för att få det att fungera
+
+#Omvandling Kelvin -> C
+K = float(input("ange temperaturen i Kelvin"))
+C = K-273.15
+print(f"{K}Kelvin = {C}C")
+
+#Omvandling C -> Kelvin
+#C = float(input("ange temperaturen i C"))
+#K = C+273.15
+#print(f"{C}C = {K}Kelvin")
+
+#Västtrafik biljetter
+#Å = int(input("Ange hur många gånger du åker kollektivtrafik under en månad:"))
+#K1 = Å*30
+#print(f"Du har åkt {Å} gånger denna månad för totalt {K1} kronor")
+
+#if(K1>775):
+    #print("Det är mer prisvärt att köpa en månadsbiljett om man åker mer än 26 gånger per månad")
