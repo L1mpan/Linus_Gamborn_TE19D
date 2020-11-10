844c68e0290fb7f0bc4291c4bd769a32017e3808
L1mpan <70134910+L1mpan@users.noreply.github.com>
2020-11-10 12:41:27 +0100

    lab 1

---
+++ genomgångar.py
@@ -0,0 +1,4 @@
+s = 0
+for n in range(1,21):
+    s = s + n
+print(f"{s}")
---
+++ laxa3.py
@@ -0,0 +1,12 @@
+# Skriv ut talen 1,2,...,10
+#for i in range(1,11):
+#    print(i, end =" ")
+
+# Skriv ut talen -10,-9,...,1,2,...,10
+#for i in range(1,22):
+#    print(i-11, end =" ")
+
+# Skriv ut talen 10,9,...,1
+#for i in range(1,11):
+#    print(11-i, end =" ")
+
